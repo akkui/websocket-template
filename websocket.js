1bb1b5e74220721cd15d61cdc911c4103a2bb9d4
kukui <97999463+akkui@users.noreply.github.com>
2023-04-30 19:54:04 -0300

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -2,15 +2,15 @@
 const socket = new WebSocket("ws://localhost:8080/");
 
 socket.onopen = function (e) {
-    socket.send(JSON.stringify({ a: 'b' }));
+    socket.send(JSON.stringify({ message: 'Connection has been opened.' }));
 };
 
 socket.onmessage = function (event) {
-    console.log(`Server response: ${event.data}`);
+    console.log("Server Responde: " + event.data);
 };
 
 socket.onclose = function (event) {
-    console.log('Connection closed.')
+    console.log('Connection has been closed.')
 };
 
 socket.onerror = function (error) {
